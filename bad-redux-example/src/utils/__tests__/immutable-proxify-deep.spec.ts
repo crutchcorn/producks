f810b78709c003127a1af340801b6441f6aa8bd7
Corbin Crutchley <crutchcorn@gmail.com>
2021-07-13 19:53:03 -0700

    Added other initial tests to immutable proxy code

--- bad-redux-example/src/utils/__tests__/immutable-proxify-deep.spec.ts
+++ bad-redux-example/src/utils/__tests__/immutable-proxify-deep.spec.ts
@@ -14,6 +14,15 @@ beforeEach(() => {
 })
 
 describe('Immutable proxify deep', () => {
+    test('Proxifing should not mutate the original in any way', () => {
+        const proxyObj = immutableProxifyDeep(obj);
+        expect(proxyObj).not.toBe(obj);
+        expect(proxyObj.lv1).not.toBe(obj.lv1);
+        expect(proxyObj.lv1.lv2).not.toBe(obj.lv1.lv2);
+        expect(obj.num).toBe(12);
+        expect(proxyObj.lv1.lv2.lv3).toBe(1);
+    })
+
     test('Should reflect data from promise to original', () => {
         const proxyObj = immutableProxifyDeep(obj);
         expect(proxyObj.num).toBe(12);
@@ -30,5 +39,19 @@ describe('Immutable proxify deep', () => {
         proxyObj.lv1.lv2.lv3 = 999;
         expect(proxyObj.lv1.lv2.lv3).toBe(999);
         expect(obj.lv1.lv2.lv3).toBe(999);
+    })
+
+    test('should create new subproxy when object added', () => {
+        const proxyObj = immutableProxifyDeep(obj);
+        expect(proxyObj.num).toBe(12);
+        expect(obj.num).toBe(12);
+        proxyObj.num = {
+            hello: 9
+        }
+        expect(proxyObj.num.hello).toBe(9);
+        expect(obj.num.hello).toBe(9);
+        proxyObj.num.hello = 1;
+        expect(proxyObj.num.hello).toBe(1);
+        expect(obj.num.hello).toBe(1);
     })
 })
